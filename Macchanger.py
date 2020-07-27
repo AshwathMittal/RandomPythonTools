ec0d8c0f253d644c80a0c15b130500fe64f67cb9
AshwathMittal <68093378+AshwathMittal@users.noreply.github.com>
2020-07-27 08:52:57 -0400

    Update Macchanger.py

--- Macchanger.py
+++ Macchanger.py
@@ -9,10 +9,10 @@ print(''' Author:Ashwath Mittal
 interface = raw_input("Interface (eth0,wlan)>:")
 new_mac = raw_input("Please enter new mac:")
 
-subprocess.call("ifconfig" + interface + "down", shell=True)
-subprocess.call("ifconfig" + interface + "hw ether" + new_mac, shell=True)
-subprocess.call("ifconfig" + interface + "up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether" + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
 
 print("changing mac for" + interface +"to" + new_mac)
 
-subprocess.call("ifconfig" + interface, shell=True)
+subprocess.call("ifconfig " + interface, shell=True)
