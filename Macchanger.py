de0cf9c39458cc251f59f1e3df8d1706c929763d
AshwathMittal <68093378+AshwathMittal@users.noreply.github.com>
2020-07-27 23:28:28 -0400

    Update Macchanger.py

--- Macchanger.py
+++ Macchanger.py
@@ -3,14 +3,15 @@
 import subprocess
 
 print(''' Author:Ashwath Mittal
-           ## Legall Disclaimer Developers or anyone associated with this Repository assume no liablity                                                  
+           ## Legall Disclaimer: Developers or anyone associated with this Repository assume no liablity                                                  
            and they are not responsible for how you use this tool.Please use this tool for educational purpose only. ''' )
 
-
+Interface = raw_input("Please enter the Interface:")
 new_mac = raw_input("Please enter new mac:")
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig " + Interface + " down", shell=True)
+subprocess.call("ifconfig " + Interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig "+ Interface + " up", shell=True)
 
 print("Your Mac is changed")
+
